ad5b43366777d1f8a2412c729c2cc109c551def8
hg2411 <himanigoyal764@gmail.com>
2025-05-27 19:27:00 +0530

    built company model schema

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -42,5 +42,5 @@ const jobSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'Application',
     }]
-});
+},{timestamps:true});
 export const Job = mongoose.model('Job',jobSchema);
