f78a46660b4a8541d9a4a4991013166a8c86c4c9
Mudit024 <jawaramudit123@gmail.com>
2025-05-28 17:51:31 +0530

    job controller updated api testing done

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,19 +2,24 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token=req.cookies.token;
-        if(!token){
-            return res.status(401).json({ message: "Unauthorized access", success: false });
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                message: "User not authenticated",
+                success: false,
+            })
         }
-        const decode =await jwt.verify(token, process.env.SECRET_KEY);
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
         if(!decode){
-            return res.status(401).json({ message: "Invalid or expired token", success: false });
-        }
-    req.id=decode.userId;
-    next();
-
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false
+            })
+        };
+        req.id = decode.userId;
+        next();
     } catch (error) {
-        console.log("JWT verification failed:", error);
+        console.log(error);
     }
-};
+}
 export default isAuthenticated;
