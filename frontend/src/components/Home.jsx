a162e0ee3e3f34e86bb412dcdfe9e7fa1868051f
hg2411 <himanigoyal764@gmail.com>
2025-06-10 14:56:35 +0530

    built herosection

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './shared/Navbar.jsx'
+import HeroSection from './HeroSection.jsx'
 
 const Home= () => {
     return (
         <div>
             <Navbar/>
+            <HeroSection/>
+            {/* <CategoryCarousel/>
+            <Footer/> */}
         </div>
     )
 }
