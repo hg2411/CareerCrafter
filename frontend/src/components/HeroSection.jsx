90af9059df36fc6264f1957e561701ef883ced6a
hg2411 <himanigoyal764@gmail.com>
2025-06-16 01:37:04 +0530

    search bar is now working to find jobs

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Search } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { setSearchedQuery } from "@/redux/jobSlice";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const [query,setQuery] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const searchJobHandler = () => {
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+  }
   return (
     <div className="flex flex-col gap-5 my-10 text-center">
   <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium">
@@ -17,9 +27,12 @@ const HeroSection = () => {
     <input
       type="text"
       placeholder="Search your next big opportunity"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
       className="flex-grow px-4 py-3 text-sm text-gray-700 outline-none bg-transparent"
     />
     <Button
+      onClick={searchJobHandler}
       type="submit"
       className="h-full px-4 rounded-none bg-gradient-to-r from-[#6A38C2] to-[#9D50BB] hover:opacity-90 text-white"
       style={{ borderRadius: '0 9999px 9999px 0' }}
